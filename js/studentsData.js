e7395cfebdf3a57b5255af6b4c9ebd65a090d310
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-03-13 19:12:27 -0700

    add student data file

---
+++ js/studentsData.js
@@ -0,0 +1,327 @@
+const studentData =[
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/67.jpg",
+                "name" : "iboya vat",
+                "email" : "iboya.vat@example.com",
+                "date joined" : "Joined 07/15/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+                "name" : "aapo niskanen",
+                "email" : "aapo.niskanen@example.com",
+                "date joined" : "Joined 06/15/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+                "name" : "phillip cox",
+                "email" : "phillip.cox@example.com",
+                "date joined" : "Joined 09/11/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/38.jpg",
+                "name" : "zilda moreira",
+                "email" : "zilda.moreira@example.com",
+                "date joined" : "Joined 07/15/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/23.jpg",
+                "name" : "lilou le gall",
+                "email" : "lilou.le gall@example.com",
+                "date joined" : "Joined 06/16/13"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/94.jpg",
+                "name" : "lucy hall",
+                "email" : "lucy.hall@example.com",
+                "date joined" : "Joined 09/11/16"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/24.jpg",
+                "name" : "mark colin",
+                "email" : "mark.colin@example.com",
+                "date joined" : "Joined 01/14/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/14.jpg",
+                "name" : "sara alves",
+                "email" : "sara.alves@example.com",
+                "date joined" : "Joined 07/19/16"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+                "name" : "ramon macrae",
+                "email" : "ramon.macrae@example.com",
+                "date joined" : "Joined 05/13/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/15.jpg",
+                "name" : "connor taylor",
+                "email" : "connor.taylor@example.com",
+                "date joined" : "Joined 05/18/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/11.jpg",
+                "name" : "aymeric morel",
+                "email" : "aymeric.morel@example.com",
+                "date joined" : "Joined 06/13/13"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+                "name" : "lorenz otto",
+                "email" : "lorenz.otto@example.com",
+                "date joined" : "Joined 05/11/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/29.jpg",
+                "name" : "karl williamson",
+                "email" : "karl.williamson@example.com",
+                "date joined" : "Joined 01/12/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+                "name" : "ouassim heering",
+                "email" : "ouassim.heering@example.com",
+                "date joined" : "Joined 01/18/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/91.jpg",
+                "name" : "roberto molina",
+                "email" : "roberto.molina@example.com",
+                "date joined" : "Joined 06/13/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/28.jpg",
+                "name" : "jordan hubert",
+                "email" : "jordan.hubert@example.com",
+                "date joined" : "Joined 06/13/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/29.jpg",
+                "name" : "melvin baker",
+                "email" : "melvin.baker@example.com",
+                "date joined" : "Joined 09/18/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/26.jpg",
+                "name" : "everett gordon",
+                "email" : "everett.gordon@example.com",
+                "date joined" : "Joined 06/17/15"
+                },  
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/82.jpg",
+                "name" : "aiden ma",
+                "email" : "aiden.ma@example.com",
+                "date joined" : "Joined 07/18/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/62.jpg",
+                "name" : "florent gerard",
+                "email" : "florent.gerard@example.com",
+                "date joined" : "Joined 02/12/13"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/96.jpg",
+                "name" : "amber chen",
+                "email" : "amber.chen@example.com",
+                "date joined" : "Joined 07/12/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/9.jpg",
+                "name" : "alexandra davies",
+                "email" : "alexandra.davies@example.com",
+                "date joined" : "Joined 05/11/13"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/57.jpg",
+                "name" : "sergio cole",
+                "email" : "sergio.cole@example.com",
+                "date joined" : "Joined 02/17/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/98.jpg",
+                "name" : "edgar dixon",
+                "email" : "edgar.dixon@example.com",
+                "date joined" : "Joined 06/17/11"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/94.jpg",
+                "name" : "kirk myers",
+                "email" : "kirk.myers@example.com",
+                "date joined" : "Joined 09/17/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/28.jpg",
+                "name" : "ani hesseling",
+                "email" : "ani.hesseling@example.com",
+                "date joined" : "Joined 08/14/16"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/24.jpg",
+                "name" : "victoire bonnet",
+                "email" : "victoire.bonnet@example.com",
+                "date joined" : "Joined 05/13/16"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/44.jpg",
+                "name" : "marcos morales",
+                "email" : "marcos.morales@example.com",
+                "date joined" : "Joined 01/12/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+                "name" : "nils neumann",
+                "email" : "nils.neumann@example.com",
+                "date joined" : "Joined 03/11/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/86.jpg",
+                "name" : "emily harrison",
+                "email" : "emily.harrison@example.com",
+                "date joined" : "Joined 07/18/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/41.jpg",
+                "name" : "matthew fortin",
+                "email" : "matthew.fortin@example.com",
+                "date joined" : "Joined 03/18/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/32.jpg",
+                "name" : "charlotte steward",
+                "email" : "charlotte.steward@example.com",
+                "date joined" : "Joined 02/18/11"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/3.jpg",
+                "name" : "marceau rodriguez",
+                "email" : "marceau.rodriguez@example.com",
+                "date joined" : "Joined 07/13/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/91.jpg",
+                "name" : "hudson anderson",
+                "email" : "hudson.anderson@example.com",
+                "date joined" : "Joined 09/12/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/33.jpg",
+                "name" : "warren phillips",
+                "email" : "warren.phillips@example.com",
+                "date joined" : "Joined 01/11/12"
+                }, 
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/41.jpg",
+                "name" : "leo niva",
+                "email" : "leo.niva@example.com",
+                "date joined" : "Joined 09/14/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/3.jpg",
+                "name" : "hani prevoo",
+                "email" : "hani.prevoo@example.com",
+                "date joined" : "Joined 01/11/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/79.jpg",
+                "name" : "veronica rodriguez",
+                "email" : "veronica.rodriguez@example.com",
+                "date joined" : "Joined 07/17/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/41.jpg",
+                "name" : "ginestal das neves",
+                "email" : "ginestal.das neves@example.com",
+                "date joined" : "Joined 06/19/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/63.jpg",
+                "name" : "devon barnes",
+                "email" : "devon.barnes@example.com",
+                "date joined" : "Joined 09/19/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/55.jpg",
+                "name" : "brennan pierce",
+                "email" : "brennan.pierce@example.com",
+                "date joined" : "Joined 05/15/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/35.jpg",
+                "name" : "zachary singh",
+                "email" : "zachary.singh@example.com",
+                "date joined" : "Joined 05/19/11"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/35.jpg",
+                "name" : "arlo harris",
+                "email" : "arlo.harris@example.com",
+                "date joined" : "Joined 07/12/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/60.jpg",
+                "name" : "hannah ginnish",
+                "email" : "hannah.ginnish@example.com",
+                "date joined" : "Joined 05/17/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/95.jpg",
+                "name" : "goos brunt",
+                "email" : "goos.brunt@example.com",
+                "date joined" : "Joined 09/15/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/91.jpg",
+                "name" : "eduard riedel",
+                "email" : "eduard.riedel@example.com",
+                "date joined" : "Joined 06/12/15"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/42.jpg",
+                "name" : "geesken jekel",
+                "email" : "geesken.jekel@example.com",
+                "date joined" : "Joined 02/12/13"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/54.jpg",
+                "name" : "dolores ryan",
+                "email" : "dolores.ryan@example.com",
+                "date joined" : "Joined 06/17/16"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+                "name" : "steven rogers",
+                "email" : "steven.rogers@example.com",
+                "date joined" : "Joined 01/18/11"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/63.jpg",
+                "name" : "virgulino silva",
+                "email" : "virgulino.silva@example.com",
+                "date joined" : "Joined 03/18/11"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/11.jpg",
+                "name" : "lucile bertrand",
+                "email" : "lucile.bertrand@example.com",
+                "date joined" : "Joined 06/13/16"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/10.jpg",
+                "name" : "elsa lahti",
+                "email" : "elsa.lahti@example.com",
+                "date joined" : "Joined 04/15/12"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/women/12.jpg",
+                "name" : "soline leclercq",
+                "email" : "soline.leclercq@example.com",
+                "date joined" : "Joined 05/12/14"
+                },
+                {
+                "img" : "https://randomuser.me/api/portraits/thumb/men/89.jpg",
+                "name" : "henri kruse",
+                "email" : "henri.kruse@example.com",
+                "date joined" : "Joined 05/14/13"
+                } 
+        
+]  
